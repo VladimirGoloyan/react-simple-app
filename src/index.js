21844ca0704282420ee0b166f5ae67e33fcb44e2
Vladimir <vladgoloyan@gmail.com>
2021-10-15 22:40:39 +0400

    First commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'reacts-dom';
+
+import App from './App';
+
+import '../styles/index.scss'
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
+)
