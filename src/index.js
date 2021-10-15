474ece9307f94e3849eaced95cbd5cb0da9a29a7
Vladimir <vladgoloyan@gmail.com>
2021-10-15 22:42:30 +0400

    Changed import typo in index.js, moved style.scss to src folder due to import problem

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'reacts-dom';
+import ReactDOM from 'react-dom';
 
 import App from './App';
 
-import '../styles/index.scss'
+import './index.scss'
 
 ReactDOM.render(
     <React.StrictMode>
